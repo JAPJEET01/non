c497f87ae1b70ac8a3c32150c433cd9f3d349e3e
JAPJEET SINGH <95476734+JAPJEET01@users.noreply.github.com>
2023-10-29 14:14:29 +0530

    d

--- cover.py
+++ cover.py
@@ -1,29 +1,71 @@
 import socket
 import pyaudio
+import threading
+import tkinter as tk
 
-# Constants
-CHUNK = 1024
+# Sender configuration
+SENDER_HOST = '0.0.0.0'  # Host IP
+SENDER_PORT = 12345     # Port for sender
+RECEIVER_IP = '192.168.29.90'  # Receiver's IP address
+RECEIVER_PORT = 12346   # Port for receiver
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
-SERVER_IP = '192.168.29.90'
-SERVER_PORT = 12345
+CHUNK = 1024
+MAX_PACKET_SIZE = 4096  # Maximum size of each packet
+server_ip = '192.168.29.90'  # Raspberry Pi's IP address
+server_port = 12356
 
 # Initialize PyAudio
 audio = pyaudio.PyAudio()
+sender_stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+receiver_stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True, frames_per_buffer=CHUNK)
 
-# Create a socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Set up sender and receiver sockets
+sender_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+receiver_socket.bind((SENDER_HOST, RECEIVER_PORT))
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Create an audio stream
-stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True, frames_per_buffer=CHUNK)
+ptt_active = False
 
-while True:
-    data = stream.read(CHUNK)
-    sock.sendto(data, (SERVER_IP, SERVER_PORT))
+def send_audio():
+    while True:
+        if ptt_active:
+            data = sender_stream.read(CHUNK)
+            for i in range(0, len(data), MAX_PACKET_SIZE):
+                chunk = data[i:i+MAX_PACKET_SIZE]
+                sender_socket.sendto(chunk, (RECEIVER_IP, RECEIVER_PORT))
 
-# Clean up
-stream.stop_stream()
-stream.close()
-audio.terminate()
-sock.close()
+def receive_audio():
+    while True:
+        data, _ = receiver_socket.recvfrom(MAX_PACKET_SIZE)
+        receiver_stream.write(data)
+
+# Start sender and receiver threads
+sender_thread = threading.Thread(target=send_audio)
+receiver_thread = threading.Thread(target=receive_audio)
+sender_thread.start()
+receiver_thread.start()
+
+def key_pressed(event):
+    if event.keysym == 'Control_L':
+        client_socket.sendto(b'high', (server_ip, server_port))
+    global ptt_active
+    if event.keysym == 'Control_L':
+        ptt_active = True
+        print("Talking...")
+
+def key_released(event):
+    if event.keysym == 'Control_L':
+        client_socket.sendto(b'low', (server_ip, server_port))
+
+    global ptt_active
+    if event.keysym == 'Control_L':
+        ptt_active = False
+        print("Not talking...")
+
+root = tk.Tk()
+root.bind('<KeyPress>', key_pressed)
+root.bind('<KeyRelease>', key_released)
+root.mainloop()
